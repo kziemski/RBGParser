5589fbb597cde5cc4c8b96a8e16a8f11a973518e
shentianxiao <tianxiao@mit.edu>
2015-05-30 11:45:38 -0400

    results

--- src/parser/Parameters.java
+++ src/parser/Parameters.java
@@ -84,25 +84,25 @@ public class Parameters implements Serializable {
 	public void randomlyInitTensor() 
 	{
  		for (int i = 0; i < rank; ++i) {
-			//U[i] = Utils.getRandomNormVector(N, 1);
-			//V[i] = Utils.getRandomNormVector(M, 1);
-			//W[i] = Utils.getRandomNormVector(D, 1);
- 			U[i] = Utils.getRandomRangeVector(N,0.01);
-			V[i] = Utils.getRandomRangeVector(M,0.01);
-			W[i] = Utils.getRandomRangeVector(D,0.01);
+			U[i] = Utils.getRandomNormVector(N, 10);
+			V[i] = Utils.getRandomNormVector(M, 10);
+			W[i] = Utils.getRandomNormVector(D, 1);
+ 			//U[i] = Utils.getRandomRangeVector(N,0.01);
+			//V[i] = Utils.getRandomRangeVector(M,0.01);
+			//W[i] = Utils.getRandomRangeVector(D,0.01);
 			totalU[i] = U[i].clone();
 			totalV[i] = V[i].clone();
 			totalW[i] = W[i].clone();
 			
 			if (options.useGP) {
-				//U2[i] = Utils.getRandomNormVector(N, 10);
-				//V2[i] = Utils.getRandomNormVector(N, 10);
-				//W2[i] = Utils.getRandomNormVector(D, 1);
-				//X2[i] = Utils.getRandomNormVector(N, 10);
-				U2[i] = Utils.getRandomRangeVector(N,0.01);
-				V2[i] = Utils.getRandomRangeVector(N,0.01);
-				W2[i] = Utils.getRandomRangeVector(D,0.01);
-				X2[i] = Utils.getRandomRangeVector(N,0.01);
+				U2[i] = Utils.getRandomNormVector(N, 10);
+				V2[i] = Utils.getRandomNormVector(N, 10);
+				W2[i] = Utils.getRandomNormVector(D, 1); 
+				X2[i] = Utils.getRandomNormVector(N, 10);
+				//U2[i] = Utils.getRandomRangeVector(N,0.01);
+				//V2[i] = Utils.getRandomRangeVector(N,0.01);
+				//W2[i] = Utils.getRandomRangeVector(D,0.01);
+				//X2[i] = Utils.getRandomRangeVector(N,0.01);
 				totalU2[i] = U2[i].clone();
 				totalV2[i] = V2[i].clone();
 				totalW2[i] = W2[i].clone();
