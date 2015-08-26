87747334f87bf7265d33b1ee3cf509636ec80470
Tianxiao Shen <tianxiao@mit.edu>
2015-08-26 23:57:08 +0800

    amend

--- run.sh
+++ run.sh
@@ -7,13 +7,6 @@ args=$1
 runid=$2
 shift
 shift
+	java -classpath "bin:lib/trove.jar" -Xmx20000m parser.DependencyParser model-file:runs/$args.model.$type.$runid train train-file:data/$args.train.$type unimap-file:unimap/$args.uni.map test test-file:data/$args.test.$type pred-file:data/$args.test.$type $@ | tee runs/$args.$type.$runid.log
 
- ln -s $args.train.$type data/$args.train.$type.$runid
- ln -s $args.test.$type data/$args.test.$type.$runid
-
-	java -classpath "bin:lib/trove.jar" -Xmx20000m parser.DependencyParser model-file:runs/$args.model.$type.$runid train train-file:data/$args.train.$type.$runid unimap-file:unimap/$args.uni.map test test-file:data/$args.test.$type.$runid label:true $@ | tee runs/$args.$type.$runid.log
-
-    rm data/$args.train.$type.$runid
-    rm data/$args.test.$type.$runid
-	#rm data/$args/$args.$type.model
 
