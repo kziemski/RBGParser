dfdea3330680879d394ddbeff10401f5ab1af70f
Tianxiao Shen <tianxiao@mit.edu>
2015-10-24 14:59:36 +0800

    feat

--- src/parser/feature/SyntacticFeatureFactory.java
+++ src/parser/feature/SyntacticFeatureFactory.java
@@ -212,12 +212,12 @@ public class SyntacticFeatureFactory implements Serializable {
 		if (l0 != 0) {
 			code = createWordCodeWP(WORDFV_W0P0, l0, p0);
 			addWordFeature(code, fv);
-			
-			code = createWordCodeWP(WORDFV_W0Pp, l0, pp);
-			addWordFeature(code, fv);
-			
-			code = createWordCodeWP(WORDFV_W0Pn, l0, pn);
-			addWordFeature(code, fv);
+//			
+//			code = createWordCodeWP(WORDFV_W0Pp, l0, pp);
+//			addWordFeature(code, fv);
+//			
+//			code = createWordCodeWP(WORDFV_W0Pn, l0, pn);
+//			addWordFeature(code, fv);
 			
 			code = createWordCodeWP(WORDFV_W0P0, l0, c0);
 			addWordFeature(code, fv);
